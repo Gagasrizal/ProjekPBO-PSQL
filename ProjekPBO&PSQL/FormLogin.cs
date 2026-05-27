3f0eab8265d0eceb7877dc26bd27dbb6cc74a5c5
Muhammad Daffa <daffasirojuddin2007@gmail.com>
2026-05-27 20:14:15 +0700

    yy

--- ProjekPBO&PSQL/FormLogin.cs
+++ ProjekPBO&PSQL/FormLogin.cs
@@ -31,5 +31,31 @@ namespace ProjekPBO_PSQL
         {
 
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FormRegistrasi regis = new FormRegistrasi();
+            regis.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (username == "admin" && password == "123")
+            {
+                MessageBox.Show("Login berhasil!");
+
+                Form home = new Form();
+                home.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login gagal!");
+            }
+        }
     }
 }
