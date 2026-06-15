dd4c4b35ae6a28d97186eac3f03ac66b883d7cfa
Muhammad Daffa <daffasirojuddin2007@gmail.com>
2026-06-15 23:56:31 +0700

    menambah pertandingan

---
+++ ProjekPBO&PSQL/Models/Pertandingan.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjekPBO_PSQL.Models
+{
+    public class Pertandingan
+    {
+        private decimal _skorPutih = 0.0m;
+        private decimal _skorHitam = 0.0m;
+
+        public int IdPertandingan { get; set; }
+        public int IdKompetisi { get; set; }
+        public DateTime TanggalPertandingan { get; set; }
+        public int Babak { get; set; }
+        public int PemainPutih { get; set; }
+        public int PemainHitam { get; set; }
+        public string Hasil { get; set; } = string.Empty;
+
+        public decimal SkorPutih
+        {
+            get => _skorPutih;
+            set
+            {
+                if (value < 0.0m || value > 1.0m)
+                    throw new ArgumentException("Skor tidak valid! Rentang skor catur per babak adalah 0, 0.5, atau 1.");
+                _skorPutih = value;
+            }
+        }
+
+        public decimal SkorHitam
+        {
+            get => _skorHitam;
+            set
+            {
+                if (value < 0.0m || value > 1.0m)
+                    throw new ArgumentException("Skor tidak valid! Rentang skor catur per babak adalah 0, 0.5, atau 1.");
+                _skorHitam = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Match ID: {IdPertandingan} | Babak: {Babak} | User {PemainPutih} (P) vs User {PemainHitam} (H) | Skor: {SkorPutih} - {SkorHitam} | Hasil: {Hasil}";
+        }
+    }
+}
